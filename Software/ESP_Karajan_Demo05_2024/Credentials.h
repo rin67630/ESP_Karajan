dac462c4f4555223642759915dcdb6a7d9abd80b
RIN67630 <lazlo.lebrun@googlemail.com>
2024-05-01 07:00:55 +0200

    Create Credentials.h
    Chang it to your own credentials

---
+++ Software/ESP_Karajan_Demo05_2024/Credentials.h
@@ -0,0 +1,19 @@
+#define WIFI_SSID            "Secret"  // Change it your own SSID
+#define WIFI_PASS            "Secret"  // Change it your own Password
+
+// ***Time zones***
+#define NTP_SERVER "de.pool.ntp.org"   // Change de. to your own country
+#define TZ             1                // (utc+) TZ in hours
+#define DST_MN         60
+#define GMT_OFFSET_SEC 3600 * TZ
+#define DAYLIGHT_OFFSET_SEC 60 * DST_MN
+
+//Thinger
+#define THINGER_USERNAME           "Secret"  // Change it your own UserName
+#define DEVICE_CREDENTIALS         "Secret"  // Change it your own Credential
+
+//Location for weather
+#define OPEN_WEATHER_MAP_APP_ID      "Secret"    // Change it your own ID
+#define OPEN_WEATHER_MAP_LOCATION_ID "TownCode"  // Change it your own TownCode
+#define OPEN_WEATHER_MAP_LANGUAGE    "en"
+#define OPEN_WEATHER_MAP_UNITS       "metric"    //Americans: learn Metric ;-)
